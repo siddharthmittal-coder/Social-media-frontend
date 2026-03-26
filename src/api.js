6524d20b92b8523b0472dd8364d94ee4528f753f
siddharthmittal-coder <sidhartbindal@gmail.com>
2026-03-26 10:34:01 +0530

    Change API base URL to production endpoint

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 const API = axios.create({
-  baseURL : 'http://localhost:5000/api',
+  baseURL : 'https://social-media-backend-es0d.onrender.com/api',
 })
 export default API;
